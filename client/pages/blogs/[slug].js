62fe8f05c8cfa5d1e6c650c953658b83cce90d60
mturkben <mturkben721@gmail.com>
2021-05-20 12:49:58 +0300

    Finished

--- client/pages/blogs/[slug].js
+++ client/pages/blogs/[slug].js
@@ -1,10 +1,32 @@
-const Blog = (props) => {
-	
-	console.log(props);
+import React from "react";
+import Head from 'next/head';
+import {IoArrowBackCircleOutline} from "react-icons/io5";
+import {useRouter} from "next/router";
+
+const Blog = ({blog}) => {
+	const router = useRouter();
 	
 	return (
 		<>
-			<div> asdasd</div>
+			<Head>
+				<title> {blog.title} </title>
+			</Head>
+			
+			<header>
+				<IoArrowBackCircleOutline size={35} className="absolute top-10 left-10" onClick={()=> router.back() }/>
+			</header>
+			
+			<div className="w-1/2 mx-auto mt-4">
+				<div className="p-2">
+					<h2
+						className="text-2xl leading-relaxed tracking-wide font-mono font-bold">
+						{blog.title}
+					</h2>
+					<div style={{textIndent: 20}}>
+						{blog.post}
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
@@ -14,7 +36,7 @@ Blog.getInitialProps = async ({query}) => {
 	const _blog = await fetch(`http://localhost:3001/api/getOneBlog/${query._id}`, {
 		method: "POST"
 	}).then(res => res.json()).then(res => res).catch(err => err);
-	console.log(_blog);
+	
 	if (_blog.status === "OK") {
 		return {blog: _blog.blog}
 	} else {
